9a72a0571a9180ffd2074c92d212b4388625ddeb
Rod Johnson <johnsonroda@gmail.com>
2018-03-14 21:01:41 +1100

    Initial commit from Atomist

---
+++ src/main/java/atomist/Y2Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Y2Application.class, args);
+	}
+}
---
+++ src/test/java/atomist/Y2ApplicationTests.java
@@ -0,0 +1,16 @@
+package atomist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Y2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
